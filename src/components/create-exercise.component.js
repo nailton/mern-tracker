639a860b05367772bb162ac18960c3d51e2e4456
Nailton Sousa <nailton.osn@gmail.com>
2022-07-17 14:40:22 -0300

    Basic CRUD

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import axios from "axios";
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -22,10 +23,15 @@ export default class CreateExercises extends Component {
     }
 
     componentDidMount() {
-        this.setState({
-            users: ['test user'],
-            username: 'test user'
-        })
+        axios.get('http://localhost:5001/users')
+            .then(response => {
+                if (response.data.length > 0) {
+                    this.setState({
+                        users: response.data.map(user => user.username),
+                        username: response.data[0].username
+                    })
+                }
+            })
     }
 
     onChangeUsername(e) {
@@ -63,6 +69,9 @@ export default class CreateExercises extends Component {
         }
 
         console.log(exercise);
+
+        axios.post('http://localhost:5001/exercises/add', exercise)
+            .then(res => console.log(res.date));
 
         window.location = '/';
     }
